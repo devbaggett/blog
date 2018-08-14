2b166911acb2a1eda54bda033aa2a68a3bfaa259
devbaggett <devabaggett@gmail.com>
2018-08-13 19:10:49 -0700

    added new views and connected them to urls.py

--- apps/blog_app/views.py
+++ apps/blog_app/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
 from django.views.generic import (
     TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView)
@@ -9,6 +9,7 @@ from .models import Post, Comment
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import PostForm, CommentForm
 from django.urls import reverse_lazy
+from django.utils import timezone
 
 
 class AboutView(TemplateView):
@@ -59,3 +60,44 @@ class DraftListView(LoginRequiredMixin, ListView):
 	def get_queryset(self):
 		# can't have publication date
 		return Post.objects.filter(published_at__isnull=True).order_by('created_at')
+
+
+###########################################
+###########################################
+
+@login_required
+def post_publish(request,pk):
+	post = get_object_or_404(Post,pk=pk)
+	post.publish
+	return redirect('post_detail', pk=pk)
+
+
+@login_required
+def add_comment_to_post(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	if request.method == 'POST':
+		form = CommentForm(request.Post)
+		if form.is_valid():
+			comment = form.save(commit=False)
+			comment.post = post
+			comment.save()
+			return redirect('post_detail', pk=post.pk)
+	else:
+		form = CommentForm()
+	return render(request,'blog_app/comment_form.html', {'form':form})
+
+@login_required
+def comment_approve(request, pk):
+	comment = get_object_or_404(Comment, pk=pk)
+	comment.approve()
+	return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request,pk):
+	comment = get_object_or_404(Coment, pk=pk)
+	post_pk = comment.post.pk
+	comment.delete()
+	return redirect('post_detail', pk=post_pk)
+
+
+
